9baee97d7a3ae77711099e07b554043c90ce2d52
Jhey <macarubbo.david@gmail.com>
2018-03-24 19:21:45 +0800

    Added footer.

--- src/Components/Home.vue
+++ src/Components/Home.vue
@@ -2,17 +2,20 @@
     <div>
         <app-navigation></app-navigation>
         <app-hero></app-hero>
+        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
 import Navigation from './Layout/Navigation';
 import Hero from './Layout/Hero';
+import Footer from './Layout/Footer';
 
 export default {
     components: {
         appNavigation: Navigation,
-        appHero: Hero
+        appHero: Hero,
+        appFooter: Footer
     }
 }
 </script>
---
+++ src/Components/Layout/Footer.vue
@@ -0,0 +1,27 @@
+<template>
+    <footer class="footer">
+        <div class="container">
+            <div class="content has-text-centered">
+                <p>
+                    ©{{ copyrightYear }} by <a href="https://github.com/jheyjheyjhey">jheyjheyjhey</a> 
+                    <br>
+                    Made with <span class="has-text-danger">❤</span> using <a href="https://vuejs.org/">Vue.js!</a> 
+                </p>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            copyrightYear: new Date().getFullYear()
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
