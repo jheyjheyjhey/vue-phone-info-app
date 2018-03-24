fb919725170b7963138cfb27fb7098596003fff7
Jhey <macarubbo.david@gmail.com>
2018-03-24 20:15:31 +0800

    Added helper for search results error.

--- src/Components/Modules/SearchPhone.vue
+++ src/Components/Modules/SearchPhone.vue
@@ -5,7 +5,8 @@
             <div class="field column is-one-third">
                 <label class="label">Name</label>
                 <div class="control">
-                    <input class="input" type="text" placeholder="Search" v-model="keyword" @input="callHttpRequest">
+                    <input class="input" type="text" placeholder="Search" v-model="searchConfig.keyword" @input="callHttpRequest">
+                    <p v-if="hasError" class="help is-danger">{{ errorMessage }}</p>
                 </div>
             </div>
             <div class="column"></div>
@@ -19,26 +20,44 @@ import _ from 'lodash';
 export default {
     data () {
         return {
-            keyword: "",
-            token: "c0cea8087b2a637fc1139ba2b50c860134e60a78b72c0f7c",
-            url: "https://fonoapi.freshpixl.com/v1/getdevice"
+            searchConfig: {
+                keyword: "",
+                token: "c0cea8087b2a637fc1139ba2b50c860134e60a78b72c0f7c",
+                url: "https://fonoapi.freshpixl.com/v1/getdevice",
+            },
+            hasError: false,
+            errorMessage: ""
         }
     },
     methods: {
-        callHttpRequest: _.debounce(function (event) {
+        callHttpRequest: _.debounce(function () {
+            this.hasError = false;
+            this.errorMessage = "";
+            let keyword = this.searchConfig.keyword;
             let config = {
                 params: {
-                    token: this.token,
-                    device: event.target.value
+                    token: this.searchConfig.token,
+                    device: this.searchConfig.keyword
                 }
             };
 
-            this.$http.get(this.url, config).then(response => {
-                console.log(response.body);
-            }, response => {
-                console.log(response);
-            });
+            if (keyword != "") {
+                this.$http.get(this.searchConfig.url, config).then(response => {
+                    if (response.body.status) {
+                        this.hasError = true;
+                        this.errorMessage = response.body.message;
+                    }
+                }, response => {
+                    console.log(response);
+                });
+            }
         }, 500)
+    },
+    watch: {
+        searchConfig (val) {
+            this.hasError = false;
+            this.errorMessage = "";
+        }
     }
 }
 </script>
