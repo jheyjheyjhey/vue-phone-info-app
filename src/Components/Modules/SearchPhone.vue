29f43d81dd6d8f73627812f8bf4b1398825bf136
Jhey <macarubbo.david@gmail.com>
2018-03-24 19:49:42 +0800

    Added HTTP call on search functionality.

--- src/Components/Modules/SearchPhone.vue
+++ src/Components/Modules/SearchPhone.vue
@@ -5,7 +5,7 @@
             <div class="field column is-one-third">
                 <label class="label">Name</label>
                 <div class="control">
-                    <input class="input" type="text" placeholder="Search">
+                    <input class="input" type="text" placeholder="Search" v-model="keyword" @input="callHttpRequest">
                 </div>
             </div>
             <div class="column"></div>
@@ -15,7 +15,29 @@
 
 <script>
 export default {
-    
+    data () {
+        return {
+            keyword: "",
+            token: "c0cea8087b2a637fc1139ba2b50c860134e60a78b72c0f7c",
+            url: "https://fonoapi.freshpixl.com/v1/getdevice"
+        }
+    },
+    methods: {
+        callHttpRequest () {
+            let config = {
+                params: {
+                    token: this.token,
+                    device: event.target.value
+                }
+            };
+
+            this.$http.get(this.url, config).then(response => {
+                console.log(response.body);
+            }, response => {
+                console.log(response);
+            });
+        }
+    }
 }
 </script>
 
