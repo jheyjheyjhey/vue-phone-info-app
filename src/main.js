d176cc2562e4a25593e3dc3c7926e26cf7b49b02
Jhey <macarubbo.david@gmail.com>
2018-03-26 20:01:34 +0800

    Added routing functionality.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
+import { routes } from './routes'
+
+const router = new VueRouter({
+  routes: routes
+})
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
+  router: router,
   render: h => h(App)
 })
