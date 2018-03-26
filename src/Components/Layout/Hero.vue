7f4f1ddf0ef68fed9b66bb31134938d6f7f69ba2
Jhey <macarubbo.david@gmail.com>
2018-03-26 21:28:21 +0800

    Added latest phones on home page.

--- src/Components/Layout/Hero.vue
+++ src/Components/Layout/Hero.vue
@@ -11,6 +11,18 @@
                     Search for Phones! Brands, models, etc.
                 </h1>
                 <app-search-phone></app-search-phone>
+                <br>
+                <h1 class="title">
+                    Or check these new phones!
+                </h1>
+                <br>
+                <div v-for="(latestPhone, index) in latestPhones" :key="index" class="columns">
+                    <div class="column"></div>
+                    <div class="column is-half">
+                        <app-latest-phones :phone="latestPhone" ></app-latest-phones>
+                    </div>
+                    <div class="column"></div>
+                </div>
             </div>
         </div>
     </section>
@@ -19,10 +31,37 @@
 
 <script>
 import SearchPhone from '../Modules/SearchPhone.vue';
+import LatestPhones from '../Modules/LatestPhones.vue';
 
 export default {
+    data () {
+        return {
+            url: 'https://fonoapi.freshpixl.com/v1/getlatest',
+            latestPhones: {},
+        }
+    },
     components: {
-        appSearchPhone: SearchPhone
+        appSearchPhone: SearchPhone,
+        appLatestPhones: LatestPhones
+    },
+    created () {
+        let config = {
+            params: {
+                token: "c0cea8087b2a637fc1139ba2b50c860134e60a78b72c0f7c",
+                limit: 5
+            }
+        };
+
+        this.$http.get(this.url, config).then(response => {
+            if (!response.body.status) {
+                this.latestPhones = response.body;
+            }
+            else {
+                alert(response.body.message);
+            }
+        }, response => {
+            console.log(response);
+        });
     }
 }
 </script>
---
+++ src/Components/Modules/LatestPhones.vue
@@ -0,0 +1,34 @@
+<template>
+    <div style="padding-bottom: 15px;">
+        <div class="box">
+            <article class="media">
+                <div class="media-content">
+                    <div class="content">
+                        <span>
+                            <strong class="has-text-info">{{ phone.DeviceName }}</strong> | <small>{{ phone.announced }}</small>
+                            <hr>
+                            <p>Features</p>
+                            <p style="white-space: pre;">  {{ phone.features_c }}</p>
+                            <a class="has-text-info">Read more...</a>
+                        </span>
+                    </div>
+                </div>
+            </article>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['phone'],
+    data () {
+        return {
+            newPhone: this.phone
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
